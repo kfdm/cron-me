78673089f64e4f4dbd2edc6d6e39d44c3dfcfd69
Paul Traylor <kungfudiscomonkey@gmail.com>
2017-09-24 20:36:30 +0900

    Better arg handling and return codes
    * If cron-me is set as SHELL in a crontab, do some substitution to swap
     with /bin/sh before calling cmd.Run()
    * Attempt to return the same returncode as the child process since this
     is ment as just a wrapper program

--- cron-me.go
+++ cron-me.go
@@ -6,25 +6,51 @@ import (
 	"io"
 	"log"
 	"os"
-	"os/user"
 	"os/exec"
+	"os/user"
+	"path/filepath"
+	"syscall"
 
-	"github.com/getsentry/raven-go"
 	"github.com/fluent/fluent-logger-golang/fluent"
+	"github.com/getsentry/raven-go"
 )
 
 func main() {
+	// Remove 'cron-me' from args
+	_, a := os.Args[0], os.Args[1:]
+
+	val, _ := os.LookupEnv("SHELL")
+	if val == os.Args[0] {
+		a = os.Args
+		a[0] = "/bin/sh"
+	}
+
+	if len(a) == 0 {
+		fmt.Printf("No arguments?")
+		os.Exit(1)
+	}
+
 	user, _ := user.Current()
 
 	logger, _ := fluent.New(fluent.Config{})
 	defer logger.Close()
 
-	cmd := exec.Command(os.Args[1])
+	cmd := &exec.Cmd{
+		Path: a[0],
+		Args: a,
+	}
+	if filepath.Base(a[0]) == a[0] {
+		if lp, err := exec.LookPath(a[0]); err != nil {
+			//cmd.lookPathErr = err
+		} else {
+			cmd.Path = lp
+		}
+	}
 
 	if logger != nil {
 		_ = logger.Post("cron.start", map[string]string{
-			"Path":  cmd.Path,
-			"User": user.Username,
+			"User":    user.Username,
+			"Command": fmt.Sprintf("%v", cmd.Args),
 		})
 	}
 
@@ -33,26 +59,34 @@ func main() {
 	cmd.Stdout = io.MultiWriter(&bufout, os.Stdout)
 	cmd.Stderr = io.MultiWriter(&bufout, os.Stderr)
 
-	err := cmd.Run()
-	if err != nil {
+	rtn := cmd.Run()
+	returncode := 0
+	if rtn != nil {
+		if msg, ok := rtn.(*exec.ExitError); ok { // there is error code
+			log.Printf("Command finished with error: %v", rtn)
+			returncode = msg.Sys().(syscall.WaitStatus).ExitStatus()
+		}
+
 		if logger != nil {
 			_ = logger.Post("cron.error", map[string]string{
-		    	"Path":  cmd.Path,
-				"User": user.Username,
+				"User":       user.Username,
+				"Command":    fmt.Sprintf("%v", cmd.Args),
+				"Returncode": fmt.Sprintf("%d", returncode),
 			})
 		}
 		raven.CaptureMessageAndWait(cmd.Path, map[string]string{
-			"Args": fmt.Sprintf("%v", cmd.Args),
-			"User": user.Username,
+			"Args":       fmt.Sprintf("%v", cmd.Args),
+			"User":       user.Username,
+			"Returncode": fmt.Sprintf("%d", returncode),
 		})
-		log.Fatal(err)
 	} else {
 		if logger != nil {
 			_ = logger.Post("cron.complete", map[string]string{
-				"Path":  cmd.Path,
-				"User": user.Username,
-				"Output": bufout.String(),
+				"User":    user.Username,
+				"Command": fmt.Sprintf("%v", cmd.Args),
+				"Output":  bufout.String(),
 			})
 		}
 	}
+	os.Exit(returncode)
 }
