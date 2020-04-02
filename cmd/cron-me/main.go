467c97bf940c500cf5b3a31b87d4eba0b6d9c507
Paul Traylor <paul.traylor@linecorp.com>
2020-04-02 11:23:54 +0900

    Fix ignores

---
+++ cmd/cron-me/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/kfdm/cron-me/internal"
+)
+
+func main() {
+	// Remove 'cron-me' from args
+	_, a := os.Args[0], os.Args[1:]
+	cmd := &exec.Cmd{
+		Path: a[0],
+		Args: a,
+	}
+
+	if filepath.Base(cmd.Args[0]) == cmd.Args[0] {
+		if lp, err := exec.LookPath(cmd.Args[0]); err != nil {
+			//cmd.lookPathErr = err
+		} else {
+			cmd.Path = lp
+		}
+	}
+
+	os.Exit(internal.Run(cmd))
+}
